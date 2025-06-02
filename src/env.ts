92f3b9027052de72dca2231478d35894f016f42b
Oren Aksakal <orenaksakal@gmail.com>
2025-06-01 21:12:46 -0600

    initial commit

--- src/env.ts
+++ src/env.ts
@@ -5,9 +5,6 @@ export const env = envsafe({
   AWS_SECRET_ACCESS_KEY: str(),
   AWS_S3_BUCKET: str(),
   AWS_S3_REGION: str(),
-  BACKUP_DATABASE_URL: str({
-    desc: 'The connection string of the database to backup.'
-  }),
   BACKUP_CRON_SCHEDULE: str({
     desc: 'The cron schedule to run the backup on.',
     default: '0 5 * * *',
