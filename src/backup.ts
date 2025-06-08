8611e86a0ea109fafd7b8f2e13e09ef8ea6397b7
Oren Aksakal <orenaksakal@gmail.com>
2025-06-08 11:16:02 -0600

    Update backup.ts

--- src/backup.ts
+++ src/backup.ts
@@ -64,15 +64,7 @@ const dumpToFile = async (filePath: string, filename: string) => {
         reject({ error: error, stderr: stderr.trimEnd() });
         return;
       }
-
-      // check if archive is valid and contains data
-      // const isValidArchive = (execSync(`gzip -cd ${filePath} | head -c1`).length == 1) ? true : false;
-
-      // if (isValidArchive == false) {
-      //   reject({ error: "Backup archive file is invalid or empty; check for errors above" });
-      //   return;
-      // }
-
+      
       // not all text in stderr will be a critical error, print the error / warning
       if (stderr != "") {
         console.log({ stderr: stderr.trimEnd() });
@@ -80,12 +72,7 @@ const dumpToFile = async (filePath: string, filename: string) => {
 
       // console.log("Backup archive file is valid");
       console.log("Backup filesize:", filesize(statSync(filePath).size));
-
-      // if stderr contains text, let the user know that it was potently just a warning message
-      if (stderr != "") {
-        console.log(`Potential warnings detected; Please ensure the backup file "${path.basename(filePath)}" contains all needed data`);
-      }
-
+      
       resolve(undefined);
     });
   });
